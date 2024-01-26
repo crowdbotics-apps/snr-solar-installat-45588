a8f1b13b477f5105e5953a65b22e9392553a6daf
Crowdbotics <team@crowdbotics.com>
2024-01-26 19:16:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snr-solar-installat-45588",
+  url: "https://snr-solar-installat-45588.botics.co",
+  api: "https://snr-solar-installat-45588.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
